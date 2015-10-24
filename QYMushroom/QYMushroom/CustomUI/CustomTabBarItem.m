c712a467e68c16905266159410f18123c071bfb9
LittleBitch-x <844142553@qq.com>
2015-10-24 19:23:31 +0800

    as
    asd

---
+++ QYMushroom/QYMushroom/CustomUI/CustomTabBarItem.m
@@ -0,0 +1,21 @@
+//
+//  CustomTabBarItem.m
+//  QYMushroom
+//
+//  Created by 李祖建 on 15/10/24.
+//  Copyright © 2015年 RecordTeam. All rights reserved.
+//
+
+#import "CustomTabBarItem.h"
+
+@implementation CustomTabBarItem
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
--- QYMushroom/QYMushroom/聊聊聊/CommunityViewController.m
+++ QYMushroom/QYMushroom/聊聊聊/CommunityViewController.m
@@ -33,6 +33,7 @@
     // Get the new view controller using [segue destinationViewController].
     // Pass the selected object to the new view controller.
 }
+ 
 */
 
 @end
